ca3edbd9053d2230bf7b5f8b82bfa94b00fc4aca
Fernando José Flores Mendoza <110640664+ferflores28032002@users.noreply.github.com>
2024-11-26 19:23:34 -0600

    new file

---
+++ status.ts
@@ -0,0 +1,11 @@
+interface StatusInterface {
+  success: string;
+  error: string;
+  loading: string;
+}
+
+const states: StatusInterface = {
+  success: "success",
+  error: "error",
+  loading: "loading",
+};
